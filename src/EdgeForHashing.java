eadc66821ffa7e706b81c509816a66fdf95d8bfe
vasil <vasilg003@gmail.com>
2025-11-26 12:03:14 +0200

    Add occupied data field
    The occupied instance variable is a boolean variable
    that is true if the edge contains a label and false
    otherwise.

--- src/EdgeForHashing.java
+++ src/EdgeForHashing.java
@@ -4,10 +4,12 @@ public class EdgeForHashing {
 
     protected String label;
     protected CompressedTrieNodeWithHash child;
+    protected boolean occupied;
 
     public EdgeForHashing(String label, CompressedTrieNodeWithHash child) {
         this.label = label;
         this.child = child;
+        this.occupied = true;
     }
 
     public EdgeForHashing(String label) {
